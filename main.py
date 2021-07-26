13af4beab47f0ee6f65ab5b6313432d605166c1d
Lucas-HLF <lucashlfernandes33@gmail.com>
2021-07-26 18:04:51 -0300

    Criando os metodos para das categorias e livros

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://books.toscrape.com/catalogue/page-1.html"
+url = "https://books.toscrape.com/catalogue/category/books_1/index.html"
 source = requests.get(url).text
 
 soup = BeautifulSoup(source, 'html.parser')
 
-divs = soup.find_all("article", class_="product_pod")
+def find_books():
+    divs = soup.find_all("article", class_="product_pod")
+    books_list = []
+    for div in divs:
+        title = div.h3.text
+        price = div.find("p", class_="price_color").text
+        in_stock = div.find("p", class_="instock availability").text
+        
+        books_list.append({
+            "Title": title,
+            "Price": price.replace("Â£", "$"),
+            "Available": in_stock.strip()
+        })
 
-for div in divs:
-    title = div.h3.text
-    price = div.find("p", class_="price_color").text
-    in_stock = div.find("p", class_="instock availability").text
-    print(dict({
-        "Title": title,
-        "Price": price.replace("Â£", "$"),
-        "Available": in_stock.strip()
-    }))
+    return books_list
 
-categories = soup.find("ul", class_="nav-list").li.ul
 
-category_list = []
-for category in categories.find_all("li"):
-    category_list.append(category.text.strip())
+def find_categories():
+    categories = soup.find("ul", class_="nav-list").li.ul
 
-print(dict({
-    "Categories": [category_items for category_items in category_list]
-}))
+    categories_list = []
+    for category in categories.find_all("li"):
+        categories_list.append({
+            "Category": category.a.text.strip(),
+            "Href": category.a['href']
+        })                
+    
+    return categories_list
 
+if __name__ == "__main__":
+    
