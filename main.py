186df84f9cadeddd8bda1b0a8accebb85f423179
Lucas-HLF <lucashlfernandes33@gmail.com>
2021-07-27 17:55:44 -0300

    Passando por cada categoria e extraindo as informações

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import time
+
 
 url = "https://books.toscrape.com/catalogue/category/books_1/index.html"
 source = requests.get(url).text
 
 soup = BeautifulSoup(source, 'html.parser')
 
+
 def find_books():
-    divs = soup.find_all("article", class_="product_pod")
+    books_div = soup.find_all("article", class_="product_pod")
     books_list = []
-    for div in divs:
+    for div in books_div:
         title = div.h3.text
         price = div.find("p", class_="price_color").text
         in_stock = div.find("p", class_="instock availability").text
@@ -17,17 +20,17 @@ def find_books():
         books_list.append({
             "Title": title,
             "Price": price.replace("Â£", "$"),
-            "Available": in_stock.strip()
+            "Stock": in_stock.strip()
         })
 
     return books_list
 
 
 def find_categories():
-    categories = soup.find("ul", class_="nav-list").li.ul
+    categories_items = soup.find("ul", class_="nav-list").li.ul
 
     categories_list = []
-    for category in categories.find_all("li"):
+    for category in categories_items.find_all("li"):
         categories_list.append({
             "Category": category.a.text.strip(),
             "Href": category.a['href']
@@ -35,5 +38,17 @@ def find_categories():
     
     return categories_list
 
+
 if __name__ == "__main__":
-    
+    categories = find_categories()
+
+    for key in categories:
+        url = f"https://books.toscrape.com/catalogue/category/{key['Href'][3::]}"
+        source = requests.get(url).text
+
+        soup = BeautifulSoup(source, 'html.parser')
+
+        books = find_books()
+        print((key["Category"],
+        books))
+        time.sleep(10)
